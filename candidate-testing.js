ea844631de5705412d7a16f1173d7e50af71e829
AmieAnne <142542738+AmieAnne@users.noreply.github.com>
2023-09-23 18:34:40 -0500

    Update candidate-testing.js

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,15 +5,15 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
+let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers 
-let candidateAnswers;
+let questions = ["Who was the first American woman in space? ","True or false: 5 kilometer == 5000 meters? ","(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride","True",	"40", "Trajectory", "3"]
+let candidateAnswers = [];
 
 runProgram ();
 function askForName() {
@@ -22,22 +22,32 @@ candidateName = (input.question("What is your name? "))}
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-this.candidateAnswer = (input.question("Who was the first American woman in space? "))
-
-
-
+//this.candidateAnswer = (input.question("Who was the first American woman in space? "))
+for (let i = 0; i < questions.length; i++){
+let answer = input.question(questions[i])
+  candidateAnswers.push(answer)}
 }
 
-function gradeQuiz(candidateAnswer) {
+function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-console.log(candidateAnswer)
-  if (candidateAnswer == correctAnswer) {
-    console.log("YES! Good job, " + candidateName +".");
-  }
-  else if (candidateAnswer != correctAnswer){
-    console.log("No! Sorry," + candidateName);
-  }
+//console.log(candidateAnswers)
+  //if (candidateAnswer == correctAnswer) {
+  //  console.log("YES! Good job, " + candidateName +".");
+  //}
+  //else if (candidateAnswer != correctAnswer){
+  //  console.log("No! Sorry," + candidateName);
+  //}
+  for (let i = 0; i < candidateAnswers.length; i++)
+  {
+    if (candidateAnswers[i] == correctAnswers[i]) {
+      console.log("YES! Good job, " + candidateName +".");
+      }
+      else if (candidateAnswers[i] != correctAnswers[i]){
+
+      
+      console.log("No! Sorry, " + candidateName);
+  }}
   
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
